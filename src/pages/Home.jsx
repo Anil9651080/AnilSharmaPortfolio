70705f53ca599f689ce1c998b62ebf02e6e2c210
Anil Sharma <anilsharma9651080@gmail.com>
2023-04-30 00:00:16 +0530

    Anil Portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
 import pic1 from '../assets/pic1.jpg'
-// import pic2 from '../assets/pic2.jpg'
-// import pic3 from '../assets/pic3.jpg'
-// import pic4 from '../assets/pic4.jpg'
-// import pic5 from '../assets/pic5.jpg'
-// import pic6 from '../assets/pic6.jpg'
+
 import Particle from '../components/Particle';
 import About from '../components/Home/About';
 import Type from '../components/Home/Type';
